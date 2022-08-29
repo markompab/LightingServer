9069775df19d1cc8970ddaa6a0f1807b8ab9ae14
markompab <markompab@gmail.com>
2022-08-29 11:58:35 +0900

    Basic Server Structure

---
+++ comms/tcp_positionserver.py
@@ -0,0 +1,31 @@
+# echo-server.py
+
+import socket
+import _thread
+from _thread import *
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+def onNewClient(conn, addr):
+    with conn:
+        print(f"Connected by {addr}")
+
+        #while True:
+        data = conn.recv(1024)
+
+        result ='{\n'\
+                +'\"DevicePosition\" : \"{}\"\n'.format("0") \
+                +'\"DeviceRotation\" : \"{}\"\n'.format("0") \
+                +"}";
+
+        bresult =  str.encode(result)
+        conn.sendall(bresult)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    while True:
+        conn, addr = s.accept()
+        _thread.start_new_thread(onNewClient, (conn, addr))
+    s.close()
